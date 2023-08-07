fcfc491cdaf6d3728b0298ff4f322e3081a7b847
thdcodud01 <thdcodud01@gmail.com>
2023-08-07 10:36:44 +0900

    setting

---
+++ src/main/java/com/ll/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package com.ll.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
+	}
+
+}
